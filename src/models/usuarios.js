2c635613a1dd6d3299950b821f5a7dd8b893ab8e
Marcos Verblud <marcosverblud@gmail.com>
2022-08-18 21:25:16 -0300

    model usuario

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -11,7 +11,6 @@ const usuarioSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        unique: true
     },
     direccion: { type: String },
     edad: { type: Number },
